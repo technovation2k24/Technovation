a0644222bf0881bb4eb2be056bee0aaf8beee4f6
faiyas <technovationmec@gmail.com>
2024-09-03 23:43:57 +0530

    UP

---
+++ wp-content/plugins/image-optimization/trunk/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementor/image-optimizer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8ba6e8fd556916adad820179c43d6fbbcb32c721',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/image-optimizer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8ba6e8fd556916adad820179c43d6fbbcb32c721',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'woocommerce/action-scheduler' => array(
+            'pretty_version' => '3.8.1',
+            'version' => '3.8.1.0',
+            'reference' => 'e331b534d7de10402d7545a0de50177b874c0779',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../woocommerce/action-scheduler',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
